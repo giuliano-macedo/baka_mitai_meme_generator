f232b253fe0c8491699d15bf27a935e73a46bf53
Giuliano Oliveira <giuliano.llpinokio@gmail.com>
2020-08-07 15:13:37 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# [Bakai Mitai meme]((https://knowyourmeme.com/memes/dame-da-ne-baka-mitai)) generator using First Order Motion Model
+
+## pre-requirements
+* python >=3.6
+* pip
+* ffmpeg
+* download data (move all to `./data`):
+	* [pre-trained weights](https://mega.nz/file/4cAyTIpT#5c5n43cLo4xc_uSgyBMIxMLB1S3_tNtzeiEZALnIyyc)
+	* \[optional\] [pre-trained adversary weights](https://mega.nz/file/MNJw3ASA#ykp34kcenxKAEoTAW6__UhSrkxdchzqBq2p6qSzCkLE)
+	* [video template](https://www.kapwing.com/videos/5f2831922695a400156ada1e) (save as `template.mp3` inside the `data` folder)
+	* [audio template](https://filebin.net/tqtq7j5mvxq3j72k/dame.mp3?t=za5niv86)
+
+## instalation
+
+```shell
+pip install -r requirements.txt
+```
+
+## usage
+```shell
+./main.py [image path]
+```
+results will be saved to the folder `./results`
+
+# acknowledgements
+thanks to [mohammedri](https://github.com/mohammedri) for his awesome [First Order Motion Model repo](https://github.com/AliaksandrSiarohin/first-order-model/blob/master/LICENSE.md)
---
+++ main.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+import os
+import argparse
+
+parser=argparse.ArgumentParser()
+parser.add_argument("image")
+parser.add_argument("-a","--adversary",action="store_true")
+parser.add_argument("-r","--relative",action="store_false")
+parser.add_argument("-A","--adapt-movement-scale",action="store_false")
+
+args=parser.parse_args()
+
+assert(os.path.isfile(args.image))
+
+import imageio
+from skimage.transform import resize
+import warnings
+import sys;sys.path.append("./first-order-model")
+from demo import load_checkpoints,make_animation
+from skimage import img_as_ubyte
+from subprocess import check_output
+import shlex
+warnings.filterwarnings("ignore")
+
+def create_results_dir():
+	path="./results"
+	if os.path.exists(path) and not os.path.isdir(path):
+		raise RuntimeError(f"{os.getcwd()}/results is not a directory")
+	elif not os.path.exists(path):
+		os.mkdir(path)
+
+print("loading model,","using" if args.adversary else "not using","adversary")
+generator, kp_detector = load_checkpoints(
+	config_path='./first-order-model/config/vox-256.yaml', 
+    checkpoint_path='./data/vox-cpk.pth.tar' if not args.adversary else "./data/vox-adv-cpk.pth.tar"
+)
+
+print("loading input")
+source_image = imageio.imread(args.image)
+driving_video = imageio.mimread('data/template.mp4', memtest=False)
+
+source_image = resize(source_image, (256, 256))[..., :3]
+driving_video = [resize(frame, (256, 256))[..., :3] for frame in driving_video]
+print("making predictions")
+predictions = make_animation(
+	source_image, driving_video, generator, kp_detector,
+	relative=args.relative,
+	adapt_movement_scale=args.adapt_movement_scale
+)
+create_results_dir()
+
+fname_no_ext=os.path.splitext(os.path.split(args.image)[-1])[0]
+output_no_audio=os.path.join("results",fname_no_ext+".mp4")
+output_audio=os.path.join	("results",fname_no_ext+"_audio.mp4")
+
+print("saving",output_no_audio)
+imageio.mimsave(output_no_audio, [img_as_ubyte(frame) for frame in predictions], fps=30)
+print("adding audio",output_audio)
+
+check_output(shlex.split(
+	f"ffmpeg -y -i {output_no_audio} -i data/dame.mp3 -codec copy -shortest {output_audio}"
+))
